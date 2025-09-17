a9c0f51ad476f2adfde8a53a09cf5922175f5b60
66050158-ddeengam <66050158@kmitl.ac.th>
2025-09-18 00:16:42 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 // num2}" if num2 != 0 else "Division by zero is not allowed")
+print(f"{num1} * {num2} = {num1 * num2}")
